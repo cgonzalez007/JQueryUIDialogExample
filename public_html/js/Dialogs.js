55fb369124dd130197474439590e3687d45807b9
Chris Gonzalez <cgonzalez15@my.wctc.edu>
2017-09-12 11:12:58 -0500

    -First commit

---
+++ public_html/js/Dialogs.js
@@ -0,0 +1,17 @@
+/**
+ * Chris Gonzalez 2017
+ * This js file is where I make the addition of a JQuery UI dialog form. 
+ * @param {type} $
+ * @param {type} window
+ * @param {type} document
+ * @returns {undefined}
+ */
+(function ($, window, document) {
+
+    $(function () {
+        
+        
+        
+    });
+
+}(window.jQuery, window));
