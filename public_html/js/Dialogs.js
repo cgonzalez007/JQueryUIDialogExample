994f499b6be9da387c6d4d2b8bee79cd8b15554a
Chris Gonzalez <cgonzalez15@my.wctc.edu>
2017-09-13 01:44:41 -0500

    Second commit: -Got functionality working the way I wanted it to for demonstration purposes.
    TO DO NEXT:
    1.Fix styling of jQueryUI dialog and buttons
    2.Add JQuery validation
    3.Fix Nav fonts (look diff from actual website)
    4.House-keeping & documentation

--- public_html/js/Dialogs.js
+++ public_html/js/Dialogs.js
@@ -9,9 +9,75 @@
 (function ($, window, document) {
 
     $(function () {
-        
-        
-        
+
+        var dialog, form;
+        /*register inputs*/
+        var $newEmail = $('#newEmail');
+        var $newPassword = $('#newPassword');
+        var $firstName = $('#firstName');
+        var $lastName = $('#lastName');
+        var $registerForm = $('#registerForm');
+
+        /*log in inputs*/
+        var $email = ('#newEmail');
+        var $password = ('#password');
+        var $loginForm = $('#loginForm');
+
+        function createAccount() {
+            registerDialog.dialog("close");
+            $('#registerForm')[0].reset();
+        }
+
+        function logIn() {
+            loginDialog.dialog("close");
+            $('#loginForm')[0].reset();
+        }
+
+        loginDialog = $("#login-form").dialog({
+            autoOpen: false,
+            height: 400,
+            width: 350,
+            modal: true,
+            buttons: {
+                "Log in": logIn,
+                Cancel: function () {
+                    loginDialog.dialog("close");
+                }
+            },
+            close: function () {
+                loginDialog.dialog("close");
+            }
+        });
+
+        registerDialog = $("#register-form").dialog({
+            autoOpen: false,
+            height: 400,
+            width: 350,
+            modal: true,
+            buttons: {
+                "Create an account": createAccount,
+                Cancel: function () {
+                    registerDialog.dialog("close");
+                }
+            },
+            close: function () {
+                registerDialog.dialog("close");
+            }
+        });
+
+
+
+
+        $("#customer_login_link").button().on("click", function () {
+            loginDialog.dialog("open");
+            return false;
+        });
+
+        $("#customer_register_link").button().on("click", function () {
+            registerDialog.dialog("open");
+            return false;
+        });
+
     });
 
 }(window.jQuery, window));
