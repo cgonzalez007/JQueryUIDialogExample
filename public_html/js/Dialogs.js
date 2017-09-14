8d066bf536c9c476ee0ff5017c5e6f23c3ed0676
Chris Gonzalez <cgonzalez15@my.wctc.edu>
2017-09-14 01:06:15 -0500

    -Fixed A LOT of styling issues... -Added simple validation using JQuery validation. -Added more documentation -Cleaned up some files
    ***Pretty much done***

--- public_html/js/Dialogs.js
+++ public_html/js/Dialogs.js
@@ -1,7 +1,11 @@
 /**
- * Chris Gonzalez 2017
+ * Chris Gonzalez 2017 - JQueryUI widget project - Dialog
+ * original site: https://www.lewsstuff.com/
  * This js file is where I make the addition of a JQuery UI dialog forms for 
- * registering a new user and logging in. 
+ * registering a new user and logging in. There is some simple validation using 
+ * JQuery Validation. When inputs are "valid", the form submits. However, being 
+ * that this project was just created for demonstrating one JQueryUI widget, nothing is 
+ * actually being sent to a server. 
  * @param {type} $
  * @param {type} window
  * @param {type} document
@@ -18,58 +22,14 @@
 //        var $firstName = $('#firstName');
 //        var $lastName = $('#lastName');
 
-
-        var $registerForm = $('#registerForm');
-
-        /*unneeded right now*/
 //        var $email = ('#newEmail');
 //        var $password = ('#password');
 
-
+        /*Two dialog forms. One for registering users and one for simply logging 
+         * in.*/
+        var $registerForm = $('#registerForm');
         var $loginForm = $('#loginForm');
-        /**
-         * Closes dialog and clears inputs. In a real scenario the email and
-         * password would have to be sent to the server and verified. 
-         */
-        function createAccount() {
-            registerDialog.dialog("close");
-            $registerForm[0].reset();
-        }
-        /**
-         * Closes dialog and clears inputs. In a real scenario the inputs would
-         * be sent to the server to create a new user record. More would be
-         * done to validate/check inputs as well. 
-         */
-        function logIn() {
-            $('#loginForm').validate({
-		onkeyup: false,
-		rules: {
-			password: {
-				required: true,
-				range: [8,16]
-			},email: {
-				required: true,
-				range: [10,50]
-			}
-		},
-		messages: {
-			password: {
-				required: "Please enter your password.",
-				range: "Password must be between 10 and 16 \n\
-                                        characters."
-			},email: {
-				required: "Please enter your email.",
-				range: "Password must be between 10 and 50 \n\
-                                        characters."
-			}
-		},
-		errorContainer: $("#errorContainer"),
-		errorLabelContainer: $("#errorContainer ul"),
-		wrapper: "li"
-	});
-            loginDialog.dialog("close");
-            $loginForm[0].reset();
-        }
+
         /*Configurations for login dialog box*/
         var loginDialog = $("#login-form").dialog({
             autoOpen: false,
@@ -77,8 +37,9 @@
             width: 350,
             modal: true,
             buttons: {
-                "Log in": logIn,
-                Cancel: function () {
+                "Log in": function () {
+                    $($loginForm).submit();
+                }, Cancel: function () {
                     loginDialog.dialog("close");
                 }
             },
@@ -93,8 +54,9 @@
             width: 350,
             modal: true,
             buttons: {
-                "Create an account": createAccount,
-                Cancel: function () {
+                "Submit": function () {
+                    $($registerForm).submit();
+                }, Cancel: function () {
                     registerDialog.dialog("close");
                 }
             },
@@ -102,7 +64,11 @@
                 registerDialog.dialog("close");
             }
         });
-
+        /**
+         * On click will open appropriate dialog window with form for either 
+         * logging in, or registering new users, depending on which link was 
+         * clicked.
+         */
         $("#customer_login_link").on("click", function () {
             loginDialog.dialog("open");
             return false;
@@ -113,7 +79,93 @@
             return false;
         });
         
-        
+        /**
+         * Very simple validation for login dialog form
+         */
+        $loginForm.validate({
+            onkeyup: false,
+            rules: {
+                password: {
+                    required: true,
+                    minlength: 8,
+                    maxlength: 16
+                }, email: {
+                    required: true,
+                    minlength: 10,
+                    maxlength: 50
+                }
+            },
+            messages: {
+                password: {
+                    required: "Please enter your password.",
+                    minlength: "Invalid password",
+                    maxlength: "Invalid password"
+                }, email: {
+                    required: "Please enter your email.",
+                    minlength: "Invalid email",
+                    maxlength: "Invalid email"
+                }
+            },
+            errorContainer: $("#errorContainer"),
+            errorLabelContainer: $("#errorContainer ul"),
+            wrapper: "li"
+        });
+        /**
+         * Very simple validation for register dialog form
+         */
+        $registerForm.validate({
+            onkeyup: false,
+            rules: {
+                newPassword: {
+                    required: true,
+                    minlength: 8,
+                    maxlength: 16
+                }, newEmail: {
+                    required: true,
+                    minlength: 10,
+                    maxlength: 50
+                }, firstName: {
+                    required: true,
+                    minlength: 2,
+                    maxlength: 35
+                }, lastName: {
+                    required: true,
+                    minlength: 2,
+                    maxlength: 35
+                }
+            },
+            messages: {
+                newPassword: {
+                    required: "Please enter your password.",
+                    minlength: "Password must be between 10 and 16 \n\
+                                        characters.",
+                    maxlength: "Password must be between 10 and 16 \n\
+                                        characters."
+                }, newEmail: {
+                    required: "Please enter your email.",
+                    minlength: "Email must be between 10 and 16 \n\
+                                        characters.",
+                    maxlength: "Email must be between 10 and 16 \n\
+                                        characters."
+                }, firstName: {
+                    required: "Please enter your first name.",
+                    minlength: "First name must be between 10 and 16 \n\
+                                        characters.",
+                    maxlength: "First name must be between 10 and 16 \n\
+                                        characters."
+                }, lastName: {
+                    required: "Please enter your last name.",
+                    minlength: "Last name must be between 10 and 16 \n\
+                                        characters.",
+                    maxlength: "Last name must be between 10 and 16 \n\
+                                        characters."
+                }
+            },
+            errorContainer: $("#errorContainer"),
+            errorLabelContainer: $("#errorContainer ul"),
+            wrapper: "li"
+        });
+
 
     });
 
