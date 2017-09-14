e42129c0f96b20c7ea2dbb7b6971ffc41924b849
Chris Gonzalez <cgonzalez15@my.wctc.edu>
2017-09-13 21:04:20 -0500

    Fix dialog going behind images. Adjusted styling. Better now.
    TO DO NEXT:
    1. Further styling if need  
    2. Add JQuery validation
    4. Fix Nav fonts (look diff from actual website)
    5. House-keeping & documentation

--- public_html/js/Dialogs.js
+++ public_html/js/Dialogs.js
@@ -1,6 +1,7 @@
 /**
  * Chris Gonzalez 2017
- * This js file is where I make the addition of a JQuery UI dialog form. 
+ * This js file is where I make the addition of a JQuery UI dialog forms for 
+ * registering a new user and logging in. 
  * @param {type} $
  * @param {type} window
  * @param {type} document
@@ -10,32 +11,69 @@
 
     $(function () {
 
-        var dialog, form;
-        /*register inputs*/
-        var $newEmail = $('#newEmail');
-        var $newPassword = $('#newPassword');
-        var $firstName = $('#firstName');
-        var $lastName = $('#lastName');
+
+        /*unneeded right now*/
+//        var $newEmail = $('#newEmail');
+//        var $newPassword = $('#newPassword');
+//        var $firstName = $('#firstName');
+//        var $lastName = $('#lastName');
+
+
         var $registerForm = $('#registerForm');
 
-        /*log in inputs*/
-        var $email = ('#newEmail');
-        var $password = ('#password');
-        var $loginForm = $('#loginForm');
+        /*unneeded right now*/
+//        var $email = ('#newEmail');
+//        var $password = ('#password');
 
+
+        var $loginForm = $('#loginForm');
+        /**
+         * Closes dialog and clears inputs. In a real scenario the email and
+         * password would have to be sent to the server and verified. 
+         */
         function createAccount() {
             registerDialog.dialog("close");
-            $('#registerForm')[0].reset();
+            $registerForm[0].reset();
         }
-
+        /**
+         * Closes dialog and clears inputs. In a real scenario the inputs would
+         * be sent to the server to create a new user record. More would be
+         * done to validate/check inputs as well. 
+         */
         function logIn() {
+            $('#loginForm').validate({
+		onkeyup: false,
+		rules: {
+			password: {
+				required: true,
+				range: [8,16]
+			},email: {
+				required: true,
+				range: [10,50]
+			}
+		},
+		messages: {
+			password: {
+				required: "Please enter your password.",
+				range: "Password must be between 10 and 16 \n\
+                                        characters."
+			},email: {
+				required: "Please enter your email.",
+				range: "Password must be between 10 and 50 \n\
+                                        characters."
+			}
+		},
+		errorContainer: $("#errorContainer"),
+		errorLabelContainer: $("#errorContainer ul"),
+		wrapper: "li"
+	});
             loginDialog.dialog("close");
-            $('#loginForm')[0].reset();
+            $loginForm[0].reset();
         }
-
+        /*Configurations for login dialog box*/
         var loginDialog = $("#login-form").dialog({
             autoOpen: false,
-            height: 400,
+            height: 300,
             width: 350,
             modal: true,
             buttons: {
@@ -48,10 +86,10 @@
                 loginDialog.dialog("close");
             }
         });
-
+        /*Configurations for register dialog box*/
         var registerDialog = $("#register-form").dialog({
             autoOpen: false,
-            height: 400,
+            height: 435,
             width: 350,
             modal: true,
             buttons: {
@@ -65,20 +103,17 @@
             }
         });
 
-
-
-
         $("#customer_login_link").on("click", function () {
             loginDialog.dialog("open");
-            loginDialog.css('zIndex', 9999);
             return false;
         });
 
         $("#customer_register_link").on("click", function () {
             registerDialog.dialog("open");
-            registerDialog.css('zIndex', 9999);
             return false;
         });
+        
+        
 
     });
 
